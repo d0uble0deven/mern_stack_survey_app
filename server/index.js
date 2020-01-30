70776e16c25e51baed80dca38f78d4075f99d507
Dev Govindji <friendsofdev94@gmail.com>
2020-01-30 14:53:17 -0600

    sets up passport for google oauth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-})
-
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
@@ -13,3 +13,6 @@ app.listen(PORT)
 // https://gentle-depths-31305.herokuapp.com/
 
 // git push heroku master
+
+
+//Enter http://localhost:5000/auth/google/callback now, since that is what it will be changed to in a later lecture.
