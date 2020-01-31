21359704b4d49ba94a325df61d90acdaf4263508
Dev Govindji <friendsofdev94@gmail.com>
2020-01-31 16:14:05 -0600

    oauth

--- server/index.js
+++ server/index.js
@@ -4,8 +4,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-passport.use(new GoogleStrategy())
-
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
 
