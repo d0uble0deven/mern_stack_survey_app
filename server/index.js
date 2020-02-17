cf5aab98688efac13db0b66b244161499c2cb965
Dev Govindji <friendsofdev94@gmail.com>
2020-02-17 15:27:19 -0600

    reconfigures folder structure

--- server/index.js
+++ server/index.js
@@ -1,40 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+require('./services/passport')
 
 const app = express()
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken)
-  console.log(refreshToken)
-  console.log(profile)
-})
-)
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
-/*
-passport.use(new GoogleStrategy({
-    clientID: process.env['GOOGLE_CLIENT_ID'],
-    clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-    scope: 'profile'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // ...
-  }
-));
-*/
-
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
