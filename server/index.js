49fba55c0037a54eaa07420a5d167041f35d7db5
Dev Govindji <friendsofdev94@gmail.com>
2020-02-25 14:58:38 -0600

    sets up mongodb users collection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
-const app = express()
-
+require('./models/User')
 require('./services/passport')
-require('./routes/authRoutes')(app)
-
 
 mongoose.connect(keys.mongoURI);
+
+const app = express()
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const { Schema } = mongoose
 
+
+
+
+const userSchema = new Schema({
+    googleId: String
+
+
+})
+
+
+mongoose.model('users', userSchema)
