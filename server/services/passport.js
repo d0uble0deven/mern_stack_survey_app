66671d8f7b2e6d9d0e4e4db3f8b6acc74d099edf
Dev Govindji <friendsofdev94@gmail.com>
2020-02-26 16:46:49 -0600

    users can successfully login with a mix of mongo and oauth

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,13 +5,39 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null, user)
+        })
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save()
+
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    // if account exists, sign in user
+                    done(null, existingUser)
+                } else {
+                    // if user does not exist, make a new one
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then((user) => done(null, user))
+                }
+            })
+
 
 
     })
